97667bb1c9fdd3214091c5345de9140b79519574
Emma Leonardi - emma.leonardi2@studio.unibo.it <emma.leonardi2@studio.unibo.it>
2025-03-10 11:13:35 +0100

    Implemented div and curried div

--- src/main/scala/u02/Currying.scala
+++ src/main/scala/u02/Currying.scala
@@ -5,9 +5,14 @@ object Currying extends App :
   // standard function with no currying
   def mult(x: Double, y: Double): Double = x * y
 
+  def div(x: Double, y: Double): Double = x / y
+
   // function with currying
   // curriedMult has actually type: Double => (Double => Double)
   def curriedMult(x: Double)(y: Double): Double = x * y
+
+
+  def curriedDiv(x: Double)(y: Double): Double = x / y
 
   // slightly different syntax at the call side..
   println(mult(10, 2)) // 20
@@ -25,3 +30,22 @@ object Currying extends App :
   println(curriedMultAsFunction(10)(2)) // 20
   println(curriedMultAsFunction) // u02.Currying$$$Lambda$7/...
   println(curriedMultAsFunction(10)) // u02.Currying$$$Lambda$12/...
+
+  //Test div
+  println("div")
+  println(div(6,2)) //3
+  println(div(4,0)) //Infinity
+  println(div(-4,0)) //-Infinity
+  println(div(0,0)) //Nan
+
+  //Test curried div
+  //Test div
+  println("Curried div")
+  println(curriedDiv(6) (2)) //3
+  println(curriedDiv(4) (0)) //Infinity
+  println(curriedDiv(-4) (0)) //-Infinity
+  println(curriedDiv(0) (0)) //Nan
+  println(curriedDiv)
+  println(curriedDiv(3))
+  val a=curriedDiv(3)
+  println(a(4)) //0.75
