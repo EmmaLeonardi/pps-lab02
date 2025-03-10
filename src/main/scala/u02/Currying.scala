92870479559f7adcf692a99b67fe1713d6669c0a
Emma Leonardi - emma.leonardi2@studio.unibo.it <emma.leonardi2@studio.unibo.it>
2025-03-10 13:10:58 +0100

    Implemented curring of x<=y=z

--- src/main/scala/u02/Currying.scala
+++ src/main/scala/u02/Currying.scala
@@ -1,6 +1,9 @@
 package u02
 
-object Currying extends App :
+import scala.compiletime.ops.any.==
+import scala.compiletime.ops.int.*
+
+object Currying extends App:
 
   // standard function with no currying
   def mult(x: Double, y: Double): Double = x * y
@@ -33,19 +36,36 @@ object Currying extends App :
 
   //Test div
   println("div")
-  println(div(6,2)) //3
-  println(div(4,0)) //Infinity
-  println(div(-4,0)) //-Infinity
-  println(div(0,0)) //Nan
+  println(div(6, 2)) //3
+  println(div(4, 0)) //Infinity
+  println(div(-4, 0)) //-Infinity
+  println(div(0, 0)) //Nan
 
   //Test curried div
   //Test div
   println("Curried div")
-  println(curriedDiv(6) (2)) //3
-  println(curriedDiv(4) (0)) //Infinity
-  println(curriedDiv(-4) (0)) //-Infinity
-  println(curriedDiv(0) (0)) //Nan
+  println(curriedDiv(6)(2)) //3
+  println(curriedDiv(4)(0)) //Infinity
+  println(curriedDiv(-4)(0)) //-Infinity
+  println(curriedDiv(0)(0)) //Nan
   println(curriedDiv)
   println(curriedDiv(3))
-  val a=curriedDiv(3)
+  val a = curriedDiv(3)
   println(a(4)) //0.75
+
+  val p1: Int => Int => Int => Boolean = (x: Int) => (y: Int) => (z: Int) => (y == z) && (x <= y) //Curried lambda
+  val p2: (Int, Int, Int) => Boolean = (x: Int, y: Int, z: Int) => (y == z) && (x <= y) //Un-curried lambda
+  def p3(x: Int)(y: Int)(z: Int): Boolean = (y == z) && (x <= y) //Curried function
+  def p4(x: Int, y: Int, z: Int): Boolean = (y == z) && (x <= y) //Un-curried function
+
+  //true
+  println(p1(1)(1)(1))
+  println(p2(1,1,1))
+  println(p3(1)(1)(1))
+  println(p4(1,1,1))
+
+  //false
+  println(p1(1)(1)(2))
+  println(p2(1, 1, 2))
+  println(p3(1)(1)(2))
+  println(p4(1, 1, 2))
